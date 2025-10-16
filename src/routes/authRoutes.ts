51ea4eeb7b539dbaf7dd832c9ca963cbe0ad8f76
Aathithya <baluathithya@gmail.com>
2025-10-16 21:23:26 +0530

    created login by aathi

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,6 +4,8 @@ import { AuthController } from '../controllers/authController';
 const router=Router();
 const auth=new AuthController()
 router.post('/signup',(req,res,next)=>auth.signup(req,res,next))
+router.post('/refresh-token',(req,res,next)=>auth.RefreshToken(req,res,next))
+router.post("/login", (req, res, next) => auth.Login(req, res, next))
 router.post('/verify',(req,res,next)=>auth.verify(req,res,next))
 router.post('/confirm',(req,res,next)=>auth.confirm(req,res,next))
 export default router;
